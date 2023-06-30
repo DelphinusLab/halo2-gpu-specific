6ebf96969b356feef6c2a095923bef6c74d719f1
Heng Zhang <shindar90@gmail.com>
2023-06-30 17:24:19 +0800

    add assert for lookup permute

--- halo2_proofs/src/plonk/lookup/prover.rs
+++ halo2_proofs/src/plonk/lookup/prover.rs
@@ -534,6 +534,9 @@ fn permute_expression_pair<C: CurveAffine, R: RngCore>(
             i_sorted_table_coeffs += 1;
         }
     }
+
+    assert!(i_unique_input_value == unique_input_values.len());
+
     let mut permuted_table_coeffs = permuted_table_coeffs
         .iter()
         .filter_map(|x| *x)
